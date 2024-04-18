c6aa3589c91bb85a020b573ef3da9c79b6007d0d
NelsonKasunda <bsc-com-ne-08-22@unima.ac.mw>
2024-04-18 17:45:57 +0200

    Remastered integer input between 5 and 10 cpp program

--- q4IntegerInput.cpp
+++ q4IntegerInput.cpp
@@ -9,18 +9,21 @@ int main()
     cout << "Enter an integer value between 5 and 10 " <<endl;
     while (true)
     {
-        if(cin >> input && input >= 5 &&  input <= 10 ){
+        
+        cin >> input;
+        
+        if(input > 5 &&  input < 10 ){
             cout << "Your input value(" << input <<") has been accepted.";
             break;
         }
-        else if(cin >> input && input < 5 && input > 10){
+        else if(input <= 5 && input >= 10){
             cout  << "You entered " << input << "Please enter a number between 5 and 10.";
         
         }
         else{
-            cout << "Sorry, you have entered an invalid number,Please try again.";
+            cout << "Sorry, you have entered an invalid number,Please try again. \n";
             cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the input buffer
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clears the input buffer of any non-numerical values
         }
     }
     
